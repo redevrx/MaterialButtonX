ca4e2d372ffa4a0325ed8373636cb6156105c51c
unknown <kasem.jon12@hotmail.com>
2020-11-17 14:32:13 +0700

    git commit all

---
+++ lib/materialButtonX.dart
@@ -0,0 +1,90 @@
+library materialButtonX;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class MaterialButtonX extends StatelessWidget {
+  final String message;
+  final double height;
+  final double width;
+  final Color color;
+  final double iconSize;
+  final IconData icon;
+  final Function onClick;
+  final double radius;
+
+  const MaterialButtonX(
+      {Key key,
+      this.message,
+      this.height,
+      this.width,
+      this.color,
+      this.iconSize,
+      this.icon,
+      this.onClick,
+      this.radius})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onClick,
+      child: Container(
+        width: width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: color,
+                offset: Offset(.5, .5),
+                spreadRadius: 1,
+                blurRadius: 12.0)
+          ],
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(height),
+              topLeft: Radius.circular(height),
+              topRight: Radius.circular(radius / 1.5),
+              bottomRight: Radius.circular(radius / 1.5)),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              width: width - (height / 1.9),
+              height: height,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(height),
+                    topLeft: Radius.circular(height),
+                    // topRight: Radius.circular(radius),
+                    bottomRight: Radius.circular(width)),
+              ),
+              child: Text(
+                "${message}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    .copyWith(color: Colors.white),
+              ),
+            ),
+            Positioned(
+                top: 8.0,
+                bottom: 8.0,
+                right: 2.0,
+                child: Icon(
+                  icon,
+                  size: iconSize,
+                  color: color,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ test/materialButtonX_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:materialButtonX/materialButtonX.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final button = MaterialButtonX();
+    // expect(calculator.addOne(2), 3);
+    // expect(button.color, Colors.red);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
